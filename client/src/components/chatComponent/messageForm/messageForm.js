f3d808817de2b8c4121d9b341089cd7ce967314e
Fedor Lyust <loozlfc@yahoo.com>
2021-12-02 14:33:53 +0300

    socket and port fixed

--- client/src/components/chatComponent/messageForm/messageForm.js
+++ client/src/components/chatComponent/messageForm/messageForm.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Button, Container, FormControl, TextField } from '@mui/material';
+import { Button, Container, FormControl, Grid, Stack, TextField } from '@mui/material';
 
 export const MessageForm = () => {
     return (
-        <Container>
-            <FormControl sx={{ display: "flex" }}>
+        <Grid 
+            container={true}
+            justifyContent="center"
+        >
                 <TextField
                     id="message"
                     label="Write your message"
                     margin="normal"
                     name="message"
                     onChange={(e) => console.log(e.target.value)}
+                    sx={{ width: "70%" }}
                 />
                 <Button                    
-                    sx={{ mt: 3, mb: 2 }}
+                    sx={{ m: 3 }}
                     type="submit"
                     variant="contained"    
                 >
                     Send message
                 </Button>
-            </FormControl>
-        </Container>
+        </Grid>
     );
 };
